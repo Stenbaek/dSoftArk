9bbbc78d6de437e2046e98ae5c0ebede93ae1f89
kolpher <Darken13>
2012-11-07 12:47:39 +0100

    cityimpl

---
+++ src/hotciv/standard/CityImpl.java
@@ -0,0 +1,17 @@
+package hotciv.standard;
+
+import hotciv.framework.Player;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Thomas
+ * Date: 07-11-12
+ * Time: 12:43
+ * To change this template use File | Settings | File Templates.
+ */
+public class CityImpl{
+    public CityImpl(Enum<Player>p){
+
+
+    }
+}
