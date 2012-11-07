5f21a10a5f0b678044d30b5bbaecca37c6e94166
kolpher <Darken13>
2012-11-07 12:52:43 +0100

    cityimpl nu med owner

--- src/hotciv/standard/CityImpl.java
+++ src/hotciv/standard/CityImpl.java
@@ -1,5 +1,6 @@
 package hotciv.standard;
 
+import hotciv.framework.City;
 import hotciv.framework.Player;
 
 /**
@@ -9,9 +10,31 @@ import hotciv.framework.Player;
  * Time: 12:43
  * To change this template use File | Settings | File Templates.
  */
-public class CityImpl{
-    public CityImpl(Enum<Player>p){
+public class CityImpl implements City{
 
+    public Player owner;
 
+    public CityImpl(Player p){
+             this.owner = p;
+    }
+
+    @Override
+    public Player getOwner() {
+        return this.owner;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public int getSize() {
+        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public String getProduction() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public String getWorkforceFocus() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 }
