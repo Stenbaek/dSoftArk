dc5c77037027781376510f7bdc076573098bbc03
kolpher <Darken13>
2012-11-26 13:57:51 +0100

    testepsilon

--- test/hotciv/standard/TestEpsilonCiv.java
+++ test/hotciv/standard/TestEpsilonCiv.java
@@ -1,12 +1,17 @@
 package hotciv.standard;
 
 import hotciv.factories.EpsilonFactory;
+import hotciv.framework.Game;
+import hotciv.framework.GameConstants;
+import hotciv.framework.Player;
+import hotciv.framework.Position;
+import hotciv.standard.units.Archer;
 import hotciv.standard.units.Legion;
 import hotciv.standard.units.Settler;
-import hotciv.framework.*;
-
 import hotciv.teststubs.FixedTestStubCivDieRoll;
-import org.junit.*;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /** Skeleton class for AlphaCiv test cases
@@ -70,6 +75,55 @@ public class TestEpsilonCiv {
 
         assertEquals("Legion should be the only unit left after battle, at (5,6)",game.getUnitAt(new Position(5,6)).getClass(),Legion.class);
         assertNull("Settler should be deleted at (6,6)",game.getUnitAt(new Position(6,6)));
+    }
+    /**
+     * A unit should attack another but be defeated because it's attack strength
+     * is less than the defense strength of the defender.
+     * The terrain- or adjacent friend factor should not come into play.
+     *
+     * The effect of using a fixed dice is that all randomness
+     * of using a "dice" is removed and we can test the algorithm, not the random nature
+     * of a dice.
+     *
+     * Archer attack strength 		= 2 * fixed dice(2) = 4
+     * Settler defense strength 	= 3 * fixed dice(2) = 6
+     *
+     * TODO Hvordan kan man bevise at denne test rentfaktisk tester dice effekten, når dice
+     * er fixed, og der yderligere giver den samme bonus til attacker og defender ?
+     */
+    @Test
+    public void diceFactorShouldMultiplyStrengthByTwo() {
+        game.addUnit(new Position(10,3), new Archer(Player.RED));
+        game.addUnit(new Position(10,2), new Settler(Player.BLUE));
+        game.moveUnit(new Position(10,3),new Position(10, 2));
+        assertEquals("The unit at 10,2 should still be a settler", GameConstants.SETTLER,
+                game.getUnitAt(new Position(10,2)).getTypeString());
+    }
+    /**
+     * Legion at (11,2) and friend at (11,1) attacks red archer at (10,2) with friends
+     * at (10,3) (10,1) and (9,2).
+     *
+     * The legion should have a total attack strength of:
+     * 4 + 1 = 5
+     *
+     * The archer should have a defense of:
+     * 3 + 1 + 1 + 1 = 6
+     */
+    @Test
+    public void attackStrengthOfRedArcherWithFriendOnAdjacentTile() {
+        game.addUnit(new Position(10, 3), new Archer(Player.RED));
+        game.addUnit(new Position(10, 2), new Archer(Player.RED));
+        game.addUnit(new Position(10, 1), new Archer(Player.RED));
+        game.addUnit(new Position(9, 2), new Archer(Player.RED));
+
+        game.addUnit(new Position(11, 2), new Legion(Player.BLUE));
+        game.addUnit(new Position(11, 1), new Legion(Player.BLUE));
+
+        game.endOfTurn(); // new blue player is in turn
+        boolean move = game.moveUnit(new Position(11,2), new Position(10,2));
+        assertTrue("The move should be legal", move);
+        assertEquals("There should still be a archer at 10,2", Archer.class,
+                game.getUnitAt(new Position(10,2)).getTypeString());
     }
 
 }
