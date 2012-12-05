ea9c2c5634b8897ef7d6ea7bf1d4535d1c407cdd
kolpher <Darken13>
2012-12-05 11:20:31 +0100

    testsemiciv

---
+++ test/hotciv/standard/TestSemiCiv.java
@@ -0,0 +1,11 @@
+package hotciv.standard;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Thomas
+ * Date: 05-12-12
+ * Time: 11:20
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestSemiCiv {
+}
