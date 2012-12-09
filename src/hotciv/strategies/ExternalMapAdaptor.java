42f572df5908628b8673f87016e6875987ffb2e4
kolpher <Darken13>
2012-12-09 21:15:52 +0100

    externalmapadaptor

---
+++ src/hotciv/strategies/ExternalMapAdaptor.java
@@ -0,0 +1,11 @@
+package hotciv.strategies;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Thomas
+ * Date: 09-12-12
+ * Time: 21:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class ExternalMapAdaptor {
+}
